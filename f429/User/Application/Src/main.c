42a8cfa751e7facc4d5c25d348311759c758dbea
Deadline039 <1315374252@qq.com>
2024-08-15 16:11:33 +0800

    Add f429 demo.

---
+++ f429/User/Application/Src/main.c
@@ -0,0 +1,20 @@
+/**
+ * @file    main.c
+ * @author  Deadline039
+ * @brief   主函数
+ * @version 1.0
+ * @date    2024-07-29
+ */
+
+#include "includes.h"
+
+/**
+ * @brief 主函数
+ *
+ * @return 退出码
+ */
+int main(void) {
+    bsp_init();
+
+    while (1) {}
+}
